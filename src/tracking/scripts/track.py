0f433416fc2a7658a47d5f676c422a958a9273ce
boringtao <xinghui.tao@wecorpindustries.com>
2020-07-18 17:17:16 +0100

    Init: joy

--- src/tracking/scripts/track.py
+++ src/tracking/scripts/track.py
@@ -1,35 +1,51 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
 import cv2
+from copy import deepcopy
+import getch
 import numpy as np
-import rospy
 import os
+import rospy
 import rospkg
 rospack = rospkg.RosPack()
-
+from std_msgs.msg import Int8, String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from tracking.msg import BBox, BBoxes
 
 # Helpers
 from helpers.cvlib import Detection
 detection = Detection()
 
-from helpers.keyboard import Keyboard
-keyboard = Keyboard()
 
-def Track():
+class Detect:
     def __init__(self):
-        rospy.init_node('track_node', anonymous=True)
+        rospy.init_node('detect_node', anonymous=True)
         rate = rospy.Rate(30)
-
+        
         self.bridge = CvBridge()
         self.frame = None
+        self.keypress = 0
 
         rospy.Subscriber('/stream/image', Image, self.img_callback)
-
+        rospy.Subscriber('/keypress', Int8, self.key_callback)
+        bboxes_pub = rospy.Publisher('/detection/bboxes', BBoxes, queue_size=10)
+        
+        frame_count = 0
         while not rospy.is_shutdown():
-            key = keyboard.listener()
-            print(key)
+            
+            if self.frame is not None:  
+                frame = deepcopy(self.frame)
+                centroids, bboxes = detection.detect(frame)
+
+                if len(centroids) != 0:
+                    for cent in centroids:
+                        cv2.rectangle(frame, (cent[0]-20, cent[1]-40), (cent[0]+20, cent[1]+40), (255,0,0), 1)
+
+                cv2.imshow("", frame)
+                cv2.waitKey(1)
+                frame_count = frame_count + 1
+
             rate.sleep()
 
     def img_callback(self, data):
@@ -38,11 +54,19 @@ def Track():
         except CvBridgeError as e:
             print(e)
         self.frame = cv_image
+    
+    def key_callback(self, data):
+        try:
+            print(data)
+        except CvBridgeError as e:
+            print(e)
+        self.keypress = data
 
 
 if __name__ == '__main__':
     try:
-        Track()
+        Detect()
     except rospy.ROSInterruptException:
         pass
-        
+    finally:
+        cv2.destroyAllWindows()
