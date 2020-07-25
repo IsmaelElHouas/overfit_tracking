9668b3d7811394932c2fedd40f630fa372f13a21
boringtao <xinghui.tao@wecorpindustries.com>
2020-07-25 09:45:26 +0100

    Add: multi person openpose

--- src/tracking/scripts/detect_openpose.py
+++ src/tracking/scripts/detect_openpose.py
@@ -1,46 +1,68 @@
-#!/usr/bin/env python
+#! /usr/bin/env python3
 
-import cv2
-from copy import deepcopy
-import numpy as np
-import os
 import rospy
-import rospkg
-rospack = rospkg.RosPack()
-from std_msgs.msg import String
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
-from tracking.msg import BBox, BBoxes
+from std_msgs.msg import Float64
 
-# Helpers
-from helpers.openpose import OpenPose
-detection = OpenPose()
+from copy import deepcopy
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+
+from math import *
+import numpy as np
+import time
+
+# helper
+from helpers.openpose import OpenPoseVGG
+openpose = OpenPoseVGG()
+
+POSE_PAIRS = [[1,2], [1,5], [2,3], [3,4], [5,6], [6,7],
+              [1,8], [8,9], [9,10], [1,11], [11,12], [12,13],
+              [1,0], [0,14], [14,16], [0,15], [15,17],
+              [2,17], [5,16] ]
+colors = [ [0,100,255], [0,100,255], [0,255,255], [0,100,255], [0,255,255], [0,100,255],
+         [0,255,0], [255,200,100], [255,0,255], [0,255,0], [255,200,100], [255,0,255],
+         [0,0,255], [255,0,0], [200,200,0], [255,0,0], [200,200,0], [0,0,0]]
 
 class Detect:
     def __init__(self):
         rospy.init_node('detect_node', anonymous=True)
-        rate = rospy.Rate(30)
-        
-        self.bridge = CvBridge()
+        self.rate = rospy.Rate(10)
+
+        self.bridge_object = CvBridge()
         self.frame = None
 
         rospy.Subscriber('/stream/image', Image, self.img_callback)
-        bboxes_pub = rospy.Publisher('/detection/bboxes', BBoxes, queue_size=10)
-        
-        frame_count = 0
+
         while not rospy.is_shutdown():
             if self.frame is not None:
                 frame = deepcopy(self.frame)
-                points = detection.detect(frame)
 
-                for point in points:
-                    cv2.circle(frame, point, 3, [0,0,255], -1, cv2.LINE_AA)
+                features = openpose.detectFeatures(frame)
+                # print(features.shape)
+                
+                personwiseKeypoints,  keypoints_list= openpose.detectPersonwiseKeypoints(frame)
+                for i in range(18):
+                    for n in range(len(personwiseKeypoints)):
+                        index = personwiseKeypoints[n][np.array(POSE_PAIRS[i])]
+                        if -1 in index:
+                            continue
+                        B = np.int32(keypoints_list[index.astype(int), 0])
+                        A = np.int32(keypoints_list[index.astype(int), 1])
+                        if i==0:
+                            cv2.putText(frame, str(n), (B[0], A[0]-50), cv2.FONT_HERSHEY_PLAIN, 1.0, colors[n], 2)
+                        cv2.line(frame, (B[0], A[0]), (B[1], A[1]), colors[i], 3, cv2.LINE_AA)
+                
+                # detected_keypoints = openpose.detectKeypoints(frame)
+                
+                # for i in range(18):
+                #     for j in range(len(detected_keypoints[i])):
+                #         cv2.circle(frame, detected_keypoints[i][j][0:2], 3, [0,0,255], -1, cv2.LINE_AA)
+
                 cv2.imshow("", frame)
                 cv2.waitKey(1)
-                frame_count = frame_count + 1
-
-            rate.sleep()
-
+            self.rate.sleep()
+    
     def img_callback(self, data):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data)
@@ -49,10 +71,14 @@ class Detect:
         self.frame = cv_image
 
 
-if __name__ == '__main__':
+def main():
     try:
         Detect()
-    except rospy.ROSInterruptException:
+    except KeyboardInterrupt:
         pass
     finally:
         cv2.destroyAllWindows()
+    
+
+if __name__ == '__main__':
+    main()
